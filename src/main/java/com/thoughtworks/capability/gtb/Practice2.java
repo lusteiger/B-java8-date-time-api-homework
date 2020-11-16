69b5f0e5b506d262c27d6573f16b1a8a8771a054
lusteiger <leonardoxy7@gmail.com>
2020-11-16 23:26:49 +0800

    [xinyu lu] finish Practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -11,6 +13,11 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    LocalDate friday =date.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
+    if (date.isAfter(friday) || date.equals(friday) )
+      date = date.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+    else
+      date = date.plusDays(1);
+    return date;
   }
 }
